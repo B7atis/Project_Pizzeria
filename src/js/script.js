98dc9b6be76c05fae1340951204e619fde008d5a
Bartosz Jędrzejewski-Emche <jedrek2603@gmail.com>
2021-04-01 23:26:51 +0200

    Added methods for cart to getElements

--- src/js/script.js
+++ src/js/script.js
@@ -390,6 +390,7 @@
 
       thisCart.dom.wrapper = element;
       thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+      thisCart.dom.productList = thisCart.dom.wrapper.querySelector(select.cart.productList);
     }
 
     initActions(){
@@ -401,7 +402,11 @@
     }
 
     add(menuProduct){
-      // const thisCart = this;
+      const thisCart = this;
+
+      const generatedHTML = templates.cartProduct(menuProduct);
+      const generatedDOM = utils.createDOMFromHTML(generatedHTML);
+      thisCart.dom.productList.appendChild(generatedDOM);
 
       console.log('adding product', menuProduct);
     }
