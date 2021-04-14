14db537067eea3ee088ce871796f0574902c04cd
Bartosz Jędrzejewski-Emche <jedrek2603@gmail.com>
2021-04-14 21:58:19 +0200

    Tweak the HourPicker widget

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,7 +1,7 @@
 import {templates, select} from '../settings.js';
 import AmountWidget from './AmountWidget.js';
-import DatePicker from '../components/DatePicker.js';
-import HourPicker from '../components/HourPicker.js';
+import DatePicker from './DatePicker.js';
+import HourPicker from './HourPicker.js';
 
 class Booking {
   constructor(element){
